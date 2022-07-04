5f3b786159ef0b2ba33abcf67935ce163a3bd6cd
Natanael Vieira <natanaelvieira130@gmail.com>
2022-07-04 13:26:12 -0300

    LinkedList Struct definitions and tests

--- EstruturaDeDados/DataStructs/MyLinkedList.cs
+++ EstruturaDeDados/DataStructs/MyLinkedList.cs
@@ -13,17 +13,68 @@
         #endregion
 
         #region Methods
-        //AddAt, RemoveAt,ToString, Remove, Add
 
-        public int Size() => this.count;
-        public bool IsEmpty() => this.Size() == 0;
+            public int Size() => this.count;
+            public bool IsEmpty() => this.Size() == 0;
 
-        public void Add(object element) {
-            MyNode node = new MyNode(element, null);
-            this.myLinkedList[this.count - 1].Next = node;
-            this.myLinkedList.Add(node);
-            this.count++;
-        }
+            private MyNode getNodeAt(int index) {
+                MyNode node = this.myLinkedList[0];
+                for(int i = 1;i <= index;i++) {
+                    node = this.myLinkedList[i];
+                }
+                return node;
+            }
+
+            public void Add(object element) {
+                MyNode node = new MyNode(element, null);
+                if(!this.IsEmpty())
+                    this.myLinkedList[this.count - 1].Next = node;
+                this.myLinkedList.Add(node);
+                this.count++;
+            }
+
+            public object Remove() {
+                if(!this.IsEmpty()) {
+                    object elementReturn = this.myLinkedList[this.count - 1].Element;
+                    this.myLinkedList[this.count - 2].Next = null;
+                    this.count--;
+                    return elementReturn;
+                }
+                return null;
+            }
+
+            public void AddAt(object element, int index) {
+                IndexValidation(index);
+                MyNode node = new MyNode(element,this.getNodeAt(index));
+                MyNode auxNode = this.getNodeAt(index - 1);
+                auxNode.Next = node;
+                this.count++;
+            }
+
+            public object RemoveAt(int index) {
+                IndexValidation(index);
+                object elementReturn = this.getNodeAt(index).Element;
+                MyNode auxNode = this.getNodeAt(index - 1);
+                auxNode.Next = index == this.count - 1 ? null : this.getNodeAt(index + 1);
+                this.count--;
+                return elementReturn;
+            }
+
+            private void IndexValidation(int index) {
+                if(index < 0 || index >= this.count)
+                    throw new IndexOutOfRangeException();
+            }
+
+            public override string ToString() {
+                string listString = "";
+                if(!this.IsEmpty()) {
+                    for(int i = 0;i < this.count;i++) {
+                        listString = listString + this.getNodeAt(i).Element.ToString() + ",";
+                    }
+                    listString = listString.Substring(0,listString.Length - 1);
+                }
+                return listString;
+            }
 
         #endregion
     }
--- EstruturaDeDados/DataStructs/MyNode.cs
+++ EstruturaDeDados/DataStructs/MyNode.cs
@@ -1,19 +1,22 @@
 ﻿namespace EstruturaDeDados.DataStructs {
     public class MyNode {
         #region Props
-            private MyNode next;
-            private object element;
+        private MyNode next;
+        private object element;
         #endregion
 
         #region Constructors
-            public MyNode(object element,MyNode next) {
-                this.element = element;
-                this.next = next;
-            }
+
+        public MyNode(object element,MyNode next) {
+            this.element = element;
+            this.next = next;
+        }
+
         #endregion
 
         #region Methods
-            public MyNode Next { get { return this.next; } set { this.next = value; } }
+        public MyNode Next { get { return this.next; } set { this.next = value; } }
+        public object Element {get {return this.element;} set {this.element = value; } }
         #endregion
     }
 }
